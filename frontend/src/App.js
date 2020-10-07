30ec1740afb695567e15ddf95c7a0ebb782797b5
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-07 20:54:17 +0530

    added dropdowns in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
@@ -9,12 +10,12 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to College Website</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,83 @@
 import React from 'react'
-import { Navbar, Nav, Container, NavDropdown, Button, FormControl, Form } from 'react-bootstrap'
+import logo from '../logo/lu_logo_nobg.png'
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Button,
+  FormControl,
+  Form,
+} from 'react-bootstrap'
 
 const Header = () => {
-    return (
-        <header>
-            <Navbar bg="light" variant='light' collapseOnSelect expand="lg">
-                <Container>
-                    <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart" /> Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className="fas fa-user" />Sign In</Nav.Link>
-                        </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
+  return (
+    <header>
+      <Navbar bg='light' variant='light' collapseOnSelect expand='lg'>
+        <Container>
+          <Navbar.Brand href='/'>
+            <img
+              alt='lu_logo'
+              src={logo}
+              height='60'
+              className='d-inline-block align-top'
+            />
+            <div>
+              Faculty Of <br /> Engineering and Technology
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'>
+            <i class='fa fa-bars fa-lg' aria-hidden='true' />
+          </Navbar.Toggle>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart' /> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user' />
+                Sign In
+              </Nav.Link>
+              <NavDropdown title='Departments' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='/cse'>
+                  Computer Science and Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/ec'>
+                  Electronics and Communication Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/ce'>
+                  Civil Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/me'>
+                  Mechanical Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/ee'>
+                  Electrical Engineering
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title='Administration' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='/principal'>
+                  Computer Science and Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/ec'>
+                  Electronics and Communication Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/ce'>
+                  Civil Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/me'>
+                  Mechanical Engineering
+                </NavDropdown.Item>
+                <NavDropdown.Item href='/ee'>
+                  Electrical Engineering
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
