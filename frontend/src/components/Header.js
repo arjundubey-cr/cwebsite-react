61e330fbd6783dbcdb87a121ea791d3680bd427b
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-08 15:52:15 +0530

    setup react-router and react-router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../logo/lu_logo_nobg.png'
+import { LinkContainer } from 'react-router-bootstrap'
 import {
   Navbar,
   Nav,
@@ -14,17 +15,21 @@ const Header = () => {
     <header>
       <Navbar bg='light' variant='light' collapseOnSelect expand='lg'>
         <Container>
-          <Navbar.Brand href='/'>
-            <img
-              alt='lu_logo'
-              src={logo}
-              height='60'
-              className='d-inline-block align-top'
-            />
-            <div>
-              Faculty Of <br /> Engineering and Technology
-            </div>
-          </Navbar.Brand>
+          {/* Branding */}
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img
+                alt='lu_logo'
+                src={logo}
+                height='60'
+                className='d-inline-block align-top'
+              />
+              <div>
+                Faculty Of <br /> Engineering and Technology
+              </div>
+            </Navbar.Brand>
+          </LinkContainer>
+          {/* Navbar toggle area starts */}
           <Navbar.Toggle aria-controls='basic-navbar-nav'>
             <i className='fa fa-bars fa-lg' aria-hidden='true' />
           </Navbar.Toggle>
@@ -33,35 +38,36 @@ const Header = () => {
               <Nav.Link href='/cart'>
                 <i className='fas fa-shopping-cart' /> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user' />
-                Sign In
-              </Nav.Link>
-              {/* 
-              Department-Down
-              Branch-dropdown 
-              */}
+              <LinkContainer to='/about-us'>
+                <Nav.Link>About Us</Nav.Link>
+              </LinkContainer>
+              {/* dropdown for department: CSE, ECE, ME, CE, ASH, EE */}
               <NavDropdown title='Department' id='basic-nav-dropdown'>
-                <NavDropdown.Item href='/cse'>
-                  Computer Science and Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ec'>
-                  Electronics and Communication Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ce'>
-                  Civil Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/me'>
-                  Mechanical Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ee'>
-                  Electrical Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ash'>
-                  Applied Science & Humanities
-                </NavDropdown.Item>
+                <LinkContainer to='/cse'>
+                  <NavDropdown.Item>
+                    Computer Science and Engineering
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/ece'>
+                  <NavDropdown.Item>
+                    Electronics and Communication Engineering
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/ce'>
+                  <NavDropdown.Item>Civil Engineering</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/me'>
+                  <NavDropdown.Item>Mechanical Engineering</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/ash'>
+                  <NavDropdown.Item>
+                    Applied Science and Humanities
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/ee'>
+                  <NavDropdown.Item>Electrical Engineering</NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
-              <Nav.Link href='/admission'>Admission</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
