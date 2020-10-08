0dc90b6707ac0a9adffd947d2c05b78cdde98ba7
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-08 14:18:26 +0530

    customized navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,9 +5,8 @@ import {
   Nav,
   Container,
   NavDropdown,
-  Button,
-  FormControl,
-  Form,
+  Dropdown,
+  DropdownButton,
 } from 'react-bootstrap'
 
 const Header = () => {
@@ -27,7 +26,7 @@ const Header = () => {
             </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav'>
-            <i class='fa fa-bars fa-lg' aria-hidden='true' />
+            <i className='fa fa-bars fa-lg' aria-hidden='true' />
           </Navbar.Toggle>
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
@@ -38,7 +37,11 @@ const Header = () => {
                 <i className='fas fa-user' />
                 Sign In
               </Nav.Link>
-              <NavDropdown title='Departments' id='basic-nav-dropdown'>
+              {/* 
+              Department-Down
+              Branch-dropdown 
+              */}
+              <NavDropdown title='Department' id='basic-nav-dropdown'>
                 <NavDropdown.Item href='/cse'>
                   Computer Science and Engineering
                 </NavDropdown.Item>
@@ -54,24 +57,11 @@ const Header = () => {
                 <NavDropdown.Item href='/ee'>
                   Electrical Engineering
                 </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title='Administration' id='basic-nav-dropdown'>
-                <NavDropdown.Item href='/principal'>
-                  Computer Science and Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ec'>
-                  Electronics and Communication Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ce'>
-                  Civil Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/me'>
-                  Mechanical Engineering
-                </NavDropdown.Item>
-                <NavDropdown.Item href='/ee'>
-                  Electrical Engineering
+                <NavDropdown.Item href='/ash'>
+                  Applied Science & Humanities
                 </NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link href='/admission'>Admission</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
