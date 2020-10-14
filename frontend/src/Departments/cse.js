27e78b943b82903abe8949544f2adb77ac8b16c7
Shubhendra-Bahadur <40620602+Shubhendra-Bahadur@users.noreply.github.com>
2020-10-14 16:37:12 +0530

    about content added

--- frontend/src/Departments/cse.js
+++ frontend/src/Departments/cse.js
@@ -8,6 +8,21 @@ const Cse= (props)=> {
 
     const [mainContent,setContent]=useState('')
 
+    const ChangeContent=(ContentName)=>
+    {   
+       if(ContentName==='About')
+       {
+           return <About/>
+       }
+       else if(ContentName===''){
+           return <About/>
+       }
+       else{
+           return <div>content not found</div>
+       }
+        
+    }
+
     const handlediv = (e) => {
         // const a=e.target.getAttribute('Value')
         // const [mainContent,setState]=useState(e.target.getAttribute('value'))
@@ -15,36 +30,34 @@ const Cse= (props)=> {
         console.log(mainContent);
     }
 
+    const backcolorchng=(e)=>
+    {
+        
+    }
+
     const goback = () => {
-        this.props.history.push('/');
+        props.history.push('/');
     }
         return (
             <div className="screen">
                 <div className="side">
                     <div className="sidebar">
-                        <li className="sidebar-item card "><Link to='/cse' onClick={handlediv} value='About' className="active">About</Link></li>
-                        <li className="sidebar-item card "><Link to='/cse' onClick={handlediv} value='VisionAndMission'>Visions & mission</Link></li>
-                        <li className="sidebar-item card "><Link to='/cse' onClick={handlediv} value="Faculty">Faculty</Link></li>
-                        <li className="sidebar-item card "><Link to='/cse' onClick={handlediv} value='Facilities'>Facilities</Link></li>
-                        <li className="sidebar-item card "><Link to='/cse' onClick={handlediv} value='Achievement'>Achievements</Link></li>
-                        <li className="sidebar-item card "><Link to='/cse' onClick={handlediv} value='CourseSyllabus'>Course Syllabus</Link></li>
+                        <li className="sidebar-item card " onMouseOver={backcolorchng} id="About" value='About'><Link to='/cse' onClick={handlediv} value='About' className="active" onMouseOver={backcolorchng} >About</Link></li>
+                        <li className="sidebar-item card " onMouseOver={backcolorchng} id="VisionAndMission" value='VisionAndMission'><Link to='/cse' onClick={handlediv} value='VisionAndMission' onMouseOver={backcolorchng}>Visions & mission</Link></li>
+                        <li className="sidebar-item card " onMouseOver={backcolorchng} id="Faculty" value='Faculty'><Link to='/cse' onClick={handlediv} value="Faculty" onMouseOver={backcolorchng}>Faculty</Link></li>
+                        <li className="sidebar-item card " onMouseOver={backcolorchng} id="Facilities" value='Facilities'><Link to='/cse' onClick={handlediv} value='Facilities' onMouseOver={backcolorchng}>Facilities</Link></li>
+                        <li className="sidebar-item card " onMouseOver={backcolorchng} id="Achievement" value='Achievement'><Link to='/cse' onClick={handlediv} value='Achievement' onMouseOver={backcolorchng}>Achievements</Link></li>
+                        <li className="sidebar-item card " onMouseOver={backcolorchng} id="CourseSyllabus" value='CourseSyllabus'><Link to='/cse' onClick={handlediv} value='CourseSyllabus' onMouseOver={backcolorchng}>Course Syllabus</Link></li>
                     </div>
                     <button className="btn goback btn-success m-3 ml-6 text-dark p-2 rounded" onClick={goback}>Go Back</button>
                 </div>
                 <div className="content ">
-
-                    
+                    {ChangeContent(mainContent)}  
 
                 </div>
             </div>
 
         )
-
-    // const ChangeContent=(ContentName)=>
-    // {   
-       
-        
-    // }
 }
 
 export default Cse;
