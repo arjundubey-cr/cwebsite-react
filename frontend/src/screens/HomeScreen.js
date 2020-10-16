08a20ec9fc387d9023d1b37ffd091f246a603a0a
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-16 14:19:02 +0530

    added changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
-//import axios from 'axios'
+
 function HomeScreen() {
-  const [products, setProducts] = useState([])
-
-  // useEffect(() => {
-  //   const fetchProduct = async () => {
-  //     const { data } = await axios.get('/api/products')
-  //     setProducts(data)
-  //   }
-  //   fetchProduct()
-  // }, [])
-
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      <Row></Row>
     </div>
   )
 }
