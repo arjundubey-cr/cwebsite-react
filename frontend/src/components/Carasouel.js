10c4965c63b9e69a254c3b7cdc5990f940c087a8
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-16 18:09:21 +0530

    added carasouel

---
+++ frontend/src/components/Carasouel.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import img1 from '../assets/images/faculty-back1.jpg'
+import img2 from '../assets/images/faculty-front1.jpg'
+import img3 from '../assets/images/faculty-img3.jpg'
+const Carasouel = () => {
+  const [index, setIndex] = useState(0)
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex)
+  }
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className='d-block w-100' src={img1} alt='First slide' />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='d-block w-100' src={img2} alt='Second slide' />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='d-block h-50 w-100' src={img3} alt='Third slide' />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default Carasouel
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import Product from '../components/Product'
-
+import Carasouel from '../components/Carasouel'
 function HomeScreen() {
   return (
     <div>
-      <h1>About</h1>
-      <Row></Row>
+      <Carasouel />
+      <Row>
+        <Col className='text-center'>
+          <h1 class='section-heading'>About our college</h1>
+          <hr class='section-underline' />
+          <div className='description'>
+            The Faculty of Engineering and Technology was re-established as one
+            of the Faculties of the University of Lucknow in 2017. The Faculty
+            has five branches of Engineering i.e. Civil, Electrical, Mechanical,
+            Electronics &amp; Communication and Computer Science & Engineering.
+            With an intake of 90 students in Computer Science & Engineering and
+            an intake of 60 students in each of the other four branches.
+          </div>
+        </Col>
+      </Row>
     </div>
   )
 }
