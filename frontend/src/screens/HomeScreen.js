306949dc0c3165201e782e60f1cc8dbaa0741428
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-17 23:07:05 +0530

    added branch logo

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,52 +1,26 @@
 import React from 'react'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Col, Row, Container, Button } from 'react-bootstrap'
+import { Col, Row, Container } from 'react-bootstrap'
 import Carasouel from '../components/Carasouel'
+import AboutHome from '../components/AboutHome'
 const HomeScreen = () => {
   return (
     <div>
       <Carasouel />
       <Container>
         <Row>
-          <Col className='text-center'>
-            <h1 class='section-heading'>About our college</h1>
-            <hr class='section-underline' />
-            <div className='description'>
-              The Faculty of Engineering and Technology was re-established as
-              one of the Faculties of the University of Lucknow in 2017. The
-              Faculty has five branches of Engineering i.e. Civil, Electrical,
-              Mechanical, Electronics &amp; Communication and Computer Science &
-              Engineering. The Institute is committed to generating,
-              disseminating, and preserving knowledge, and to working with
-              others to bring this knowledge to bear on the world’s great
-              challenges.
-            </div>
-            <LinkContainer to='/about'>
-              <Button className='mt-3' variant='danger' size='lg'>
-                View About Us
-              </Button>
-            </LinkContainer>
-          </Col>
+          <AboutHome />
         </Row>
         <Row>
           <Col className='text-center'>
             <h1 class='section-heading'>Department We have</h1>
             <hr class='section-underline' />
-            <div className='description'>
-              The Faculty of Engineering and Technology was re-established as
-              one of the Faculties of the University of Lucknow in 2017. The
-              Faculty has five branches of Engineering i.e. Civil, Electrical,
-              Mechanical, Electronics &amp; Communication and Computer Science &
-              Engineering. The Institute is committed to generating,
-              disseminating, and preserving knowledge, and to working with
-              others to bring this knowledge to bear on the world’s great
-              challenges.
-            </div>
-            <LinkContainer to='/about'>
-              <Button className='mt-3' variant='danger' size='lg'>
-                View More About Us
-              </Button>
-            </LinkContainer>
+            <Row>
+              <div className='description'>
+                We offer course in following 5 departments, our students combine
+                analytical rigor with curiosity, playful imagination, and an
+                appetite for solving the hardest problems in service to society.
+              </div>
+            </Row>
           </Col>
         </Row>
       </Container>
