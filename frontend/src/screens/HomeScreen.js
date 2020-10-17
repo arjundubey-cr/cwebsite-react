57b369328eeebea1c34a6e3ae101043696f136f1
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-18 00:25:17 +0530

    department cards completed

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Col, Row, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Col, Row, Container, Card } from 'react-bootstrap'
 import Carasouel from '../components/Carasouel'
 import AboutHome from '../components/AboutHome'
+// importing all the branch logos
+import cse from '../assets/logo/cse.png'
+import ce from '../assets/logo/ce.png'
+import me from '../assets/logo/me.png'
+import ece from '../assets/logo/ece.png'
+import ash from '../assets/logo/ash.png'
+import ee from '../assets/logo/ee.png'
 const HomeScreen = () => {
   return (
     <div>
@@ -20,6 +28,65 @@ const HomeScreen = () => {
                 analytical rigor with curiosity, playful imagination, and an
                 appetite for solving the hardest problems in service to society.
               </div>
+            </Row>
+            <Row>
+              <Col className='justify-content-space-between'>
+                <LinkContainer to='/cse'>
+                  <Card className='departmentCard' style={{ width: '18rem' }}>
+                    <Card.Img variant='top' src={cse} />
+                    <Card.Body>
+                      <Card.Title>Computer Science and Engineering</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </LinkContainer>
+                <LinkContainer to='/cse'>
+                  <Card className='departmentCard' style={{ width: '18rem' }}>
+                    <Card.Img variant='top' src={ce} />
+                    <Card.Body>
+                      <Card.Title>Civil Engineering</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </LinkContainer>
+
+                <LinkContainer to='/cse'>
+                  <Card className='departmentCard' style={{ width: '18rem' }}>
+                    <Card.Img variant='top' src={me} />
+                    <Card.Body>
+                      <Card.Title>Mechanical Engineering</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </LinkContainer>
+              </Col>
+              <Col>
+                <LinkContainer to='/cse'>
+                  <Card className='departmentCard' style={{ width: '18rem' }}>
+                    <Card.Img variant='top' src={ece} />
+                    <Card.Body>
+                      <Card.Title>
+                        Electronics &amp; Communication Engineering
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </LinkContainer>
+
+                <LinkContainer to='/cse'>
+                  <Card className='departmentCard' style={{ width: '18rem' }}>
+                    <Card.Img variant='top' src={ee} />
+                    <Card.Body>
+                      <Card.Title>Electrical Engineering</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </LinkContainer>
+
+                <LinkContainer to='/'>
+                  <Card className='departmentCard' style={{ width: '18rem' }}>
+                    <Card.Img variant='top' src={ash} />
+                    <Card.Body>
+                      <Card.Title>Applied Science and Humanities</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </LinkContainer>
+              </Col>
             </Row>
           </Col>
         </Row>
