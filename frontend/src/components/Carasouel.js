1dcbb1a270094092c2f929d071d8bb515bf22e76
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-17 18:38:57 +0530

    edited homescreen, carasouel, edited files to disable warnings

--- frontend/src/components/Carasouel.js
+++ frontend/src/components/Carasouel.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import img1 from '../assets/images/faculty-back1.jpg'
 import img2 from '../assets/images/faculty-front1.jpg'
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { Col, Row, Container } from 'react-bootstrap'
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Col, Row, Container, Button } from 'react-bootstrap'
 import Carasouel from '../components/Carasouel'
 const HomeScreen = () => {
   return (
@@ -15,10 +16,37 @@ const HomeScreen = () => {
               one of the Faculties of the University of Lucknow in 2017. The
               Faculty has five branches of Engineering i.e. Civil, Electrical,
               Mechanical, Electronics &amp; Communication and Computer Science &
-              Engineering. With an intake of 90 students in Computer Science &
-              Engineering and an intake of 60 students in each of the other four
-              branches.
+              Engineering. The Institute is committed to generating,
+              disseminating, and preserving knowledge, and to working with
+              others to bring this knowledge to bear on the world’s great
+              challenges.
             </div>
+            <LinkContainer to='/about'>
+              <Button className='mt-3' variant='danger' size='lg'>
+                View About Us
+              </Button>
+            </LinkContainer>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='text-center'>
+            <h1 class='section-heading'>Department We have</h1>
+            <hr class='section-underline' />
+            <div className='description'>
+              The Faculty of Engineering and Technology was re-established as
+              one of the Faculties of the University of Lucknow in 2017. The
+              Faculty has five branches of Engineering i.e. Civil, Electrical,
+              Mechanical, Electronics &amp; Communication and Computer Science &
+              Engineering. The Institute is committed to generating,
+              disseminating, and preserving knowledge, and to working with
+              others to bring this knowledge to bear on the world’s great
+              challenges.
+            </div>
+            <LinkContainer to='/about'>
+              <Button className='mt-3' variant='danger' size='lg'>
+                View More About Us
+              </Button>
+            </LinkContainer>
           </Col>
         </Row>
       </Container>
