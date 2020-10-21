8a120291da54e63c6cd7d4d49e7c02281072950c
Shubhendra-Bahadur <40620602+Shubhendra-Bahadur@users.noreply.github.com>
2020-10-22 01:14:33 +0530

    achievement added

---
+++ frontend/src/Departments/mainContent/cse/Achievement.js
@@ -0,0 +1,153 @@
+import React from 'react'
+import '../../commondesign.css'
+
+const Achievement = (props) => {
+    return (
+        <div className="container-fluid">
+            <div className="intro">
+                <h2 className="intro text-uppercase tx-blue">
+                    Achievement
+            </h2>
+                <ol>
+                    <li>Number of Patent filed: <strong>01 (Machine learning based method for optimization of mobile health services and medical resources in rural and remote area)</strong></li>
+                    <li>CSI membership</li>
+                    <li>International Association of Engineers membership</li>
+                    <li>Indian Society for Technical Education membership</li>
+                    <li>International Association of Computer Science and Information Technology membership&nbsp;</li>
+                    <li>Institute of Research Engineers and Doctors membership</li>
+                    <li>International Society for Research and Development membership</li>
+                </ol>
+            </div>
+            <div className="table-content">
+                <div className="table-responsive table-shadow"><table style={{borderCollapse: 'collapse', width: '100%'}} border="1" class="table table-bordered table-striped">
+                    <tbody>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>SN</td>
+                            <td style={{width: '19.1882%',textAlign: 'center'}}>ORGANIZATION</td>
+                            <td style={{width: '65.2214%',textAlign: 'center'}}>ACTIVITY</td>
+                            <td style={{width: '11.5314%',text: 'center'}}>DATE</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>1</td>
+                            <td style={{width: '19.1882%'}}>Tata Consultancy Services (TCS), Lucknow</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A workshop on “Communication Skills in IT Industry and Agile Methodology”.</p>
+                                <p><strong>Target Audience:</strong>CSE and ECE students</p>
+                                <p><strong>Resource Person:</strong></p>
+Mr. Vishal Seth (Academic Relationship Manager- Lucknow Region, TCS)</td>
+                            <td style={{width: '11.5314%',text: 'center'}}>07.02.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>2</td>
+                            <td style={{width: '19.1882%'}}>CETPA Infotech Pvt. Ltd., Lucknow</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A special Lecture on “Advanced Design Softwares in Mechanical and Civil Engineering”.</p>
+                                <p><strong>Target Audience:</strong>ME and CE Students</p>
+                                <p><strong>Resource Person:</strong></p>
+Mr. Navneet Singh (CETPA Infotech Pvt. Ltd., Lucknow)</td>
+                            <td style={{width: '65.2214%;'}}>09.02.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>3</td>
+                            <td style={{width: '19.1882%'}}>Analyze Infotech., Lucknow</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>An Interaction Program on “JAVA Technologies”.</p>
+                                <p><strong>Target Audience:</strong>CSE, ECE and MCA students</p>
+                                <p><strong>Resource Person:</strong></p>
+Analyze Infotech resources</td>
+                            <td style={{width: '65.2214%;'}}>09.02.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>4</td>
+                            <td style={{width: '19.1882%'}}>CETPA Infotech Pvt. Ltd., Lucknow</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A seminar on “Python Programming Language”.</p>
+                                <p><strong>Target Audience:</strong>CSE students</p>
+                                <p><strong>Resource Person:</strong></p>
+                                <p>Mr. Amit Kumar Singh (CETPA Infotech Pvt. Ltd., Lucknow)</p>
+                            </td>
+                            <td style={{width: '65.2214%;'}}>27.03.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>5</td>
+                            <td style={{width: '19.1882%'}}>Softpro India Computer Technologies Pvt. Ltd.</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A one day workshop on “Advance Java with a Mini Project”.</p>
+                                <p><strong>Target Audience:</strong>CSE students</p>
+                                <p><strong>Resource Person:</strong></p>
+                                <ol>
+                                    <li>Brijesh Mishra (Softpro India Computer Technologies Pvt. Ltd.)</li>
+                                    <li>Mr. Rohit Kumar (Softpro India Computer Technologies Pvt. Ltd.)</li>
+                                </ol>
+                            </td>
+                            <td style={{width: '65.2214%;'}}>06.04.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>6</td>
+                            <td style={{width: '19.1882%'}}>Google AI Explore Academy</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A one day workshop on “Google AI Explore Machine Learning”.</p>
+                                <p><strong>Target Audience:</strong> CSE, MCA and BCA students</p>
+                                <p><strong>Resource Person:</strong></p>
+Er. Naman Mishra (Official representative of Google Crowdsource community, India and Facilitator of Google AI Academy and Co-founder of DefCon Delhi)</td>
+                            <td style={{width: '65.2214%;'}}>21.09.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>7</td>
+                            <td style={{width: '19.1882%'}}>ETL Labs</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A workshop on “Data Science-Machine Learning and Python”.</p>
+                                <p><strong>Target Audience:</strong> CSE, MCA and BCA students</p>
+                                <p><strong>Resource Person:</strong></p>
+                                <ol>
+                                    <li>VinodTripathi (President,ETL Labs)</li>
+                                    <li>Amit Singh (IIM Ahmedabad)</li>
+                                    <li>Mr. M. Qais (PMP)</li>
+                                </ol>
+                            </td>
+                            <td style={{width: '65.2214%;'}}>28.09.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>8</td>
+                            <td style={{width: '19.1882%'}}>Digipodium Computer Consultancy Pvt. Ltd.</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>A two day workshop on “Android App Development”.</p>
+                                <p><strong>Target Audience:</strong>CSE, MCAand BCA students</p>
+                                <p><strong>Resource Person:</strong></p>
+                                <p>Zaid bin Kamiland Team (Digipodium Computer Consultancy Pvt. Ltd.)</p>
+                            </td>
+                            <td style={{width: '65.2214%;'}}>
+                                <p>12.10.2019</p>
+                                <p>and</p>
+13.10.2019</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>9</td>
+                            <td style={{width: '19.1882%'}}>TPC FoET</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>Prize distribution ceremony of TPC FoET Logo Contest.</p>
+                                <p>Winner: Sachin Singh Dosad (BCA 2<sup>nd</sup> Year, 2019-20)</p>
+                                <ol>
+                                    <li>1000 Cash prize money</li>
+                                </ol>
+Appreciation certificate</td>
+                            <td style={{width: '65.2214%;'}}>12.02.2020</td>
+                        </tr>
+                        <tr>
+                            <td style={{width: '4.05904%',textAlign: 'center'}}>10</td>
+                            <td style={{width: '19.1882%'}}>LIVEWIRE, India</td>
+                            <td style={{width: '65.2214%'}}>
+                                <p>Aptitude examination – CBAT.</p>
+                                <p><strong>Target Audience:</strong> 1<sup>st</sup>, 2<sup>nd</sup> and 3<sup>rd</sup> year students of B.Tech./B.C.A./M.C.A. except B.Tech. first year students of CE/EE/ME.</p>
+                                <p>739 students participated.</p>
+                            </td>
+                            <td style={{width: '65.2214%;'}}>15.02.2020</td>
+                        </tr>
+                    </tbody>
+                </table></div>
+            </div>
+        </div>
+    )
+}
+
+export default Achievement;
