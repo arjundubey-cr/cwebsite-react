3ef2f22f1b9e8e0957a4ec0c82443c2d830d0643
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-27 19:36:42 +0530

    life @foet added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Row, Container, Col, Card, Button } from 'react-bootstrap'
+import { Row, Container, Col, Card, Button, Image } from 'react-bootstrap'
 import Carasouel from '../components/Carasouel'
 import AboutHome from '../components/AboutHome'
 import DepartmentCard from '../components/DepartmentCard'
+import imgshowcase from '../assets/images/img-showcase.jpg'
 const HomeScreen = () => {
   return (
     <div>
@@ -11,6 +12,16 @@ const HomeScreen = () => {
         <Row>
           <AboutHome />
         </Row>
+      </Container>
+      <div className='insti-life my-3'>
+        <div className='content'>
+          <h1>Life at FOET</h1>
+          <h2>We Impact Society to make it a better world to live</h2>
+          <h3>You can be part too</h3>
+          <Button>Learn More</Button>
+        </div>
+      </div>
+      <Container>
         <Row>
           <DepartmentCard />
         </Row>
