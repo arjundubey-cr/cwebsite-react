eacfcc5a840da915bca948e4252b8e240dab3039
arjundubey-cr <arjundubeycr@gmail.com>
2020-10-28 22:27:50 +0530

    added admission screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import Cse from './Departments/cse.js'
-
+import AdmissionScreen from './screens/AdmissionScreen'
 function App() {
   return (
     <Router>
@@ -12,6 +12,7 @@ function App() {
       <main>
         <Route path='/' component={HomeScreen} exact />
         <Route path='/cse' component={Cse} exact />
+        <Route path='/admission' component={AdmissionScreen} />
       </main>
       <Footer />
     </Router>
---
+++ frontend/src/screens/AdmissionScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carasouel from '../components/Carasouel'
+
+const AdmissionScreen = () => {
+  return (
+    <div>
+      Intensely curious and driven to explore, the people of MIT value rigorous
+      analytical thinking, ingenuity, hands-on problem solving, and big new
+      ideas. Stimulating, supportive, and playful, the MIT community becomes,
+      for many students, a second home.
+    </div>
+  )
+}
+
+export default AdmissionScreen
