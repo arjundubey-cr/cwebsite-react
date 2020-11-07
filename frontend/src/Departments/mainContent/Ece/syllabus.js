cddc2fb59d59bfa049329de34ec3623707f2fe2c
Shubhendra-Bahadur <40620602+Shubhendra-Bahadur@users.noreply.github.com>
2020-11-07 17:20:33 +0530

    achievement added

--- frontend/src/Departments/mainContent/Ece/syllabus.js
+++ frontend/src/Departments/mainContent/Ece/syllabus.js
@@ -1,267 +1,133 @@
-import React from 'react'
-import '../../commondesign.css'
+import React from "react";
+import "../../commondesign.css";
 
 const CourseSyllabus = (props) => {
   return (
-    <div className='syllabus pl-2 pr-2'>
-      <h2 className='intro text-uppercase tx-blue underline'>
+    <div className="syllabus pl-2 pr-2">
+      <h2 className="intro text-uppercase tx-blue underline">
         COURSE CONTENTS OR COURSE OUTLINES
       </h2>
-      <hr className='hr1 mx-2' />
-      <div className='table-content pr-2 mx-2'>
-        <div className='table-responsive table-shadow mt-2 p-3 mt-4'>
-          <table class='table table-condensed table-bordered pub table-striped'>
+      <hr className="hr1 mx-2" />
+      <div className="table-content pr-2 mx-2">
+        <div className="table-responsive table-shadow mt-2 p-3 mt-4">
+          <table class="table table-condensed table-bordered pub table-striped">
             <thead>
-              <tr className='bg-pink table-header-background'>
-                <th className='table-header-text' width='3%'>
-                  S.No.
-                </th>
-                <th className='table-header-text'>Description</th>
-                <th className='table-header-text'>View</th>
+              <tr class="bg-dark">
+                <th className='text-white' width="3%">S.No.</th>
+                <th className='text-white' >Description</th>
+                <th className='text-white' >View</th>
               </tr>
             </thead>
-            <tbody id='tblTimeTable'>
+            <tbody id="tblTimeTable">
               <tr>
-                <td class='text-center'>1</td>
+                <td class="text-center">1</td>
                 <td>
                   B.Tech First Year Syllabus (Common for All Branches )
                   Effective from Session 2017-18
                 </td>
-                <td class='text-center'>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_dd78d5d5-8a4a-48ab-af44-6f0bbd5fcabb_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_58f3e1a3-74af-4893-845d-9fae7c3bddc0_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
               </tr>
               <tr>
-                <td class='text-center'>2</td>
+                <td class="text-center">2</td>
                 <td>
-                  B.Tech Second Year Syllabus Computer Science Engineering
+                  B.Tech Third Year Syllabus Electronics and Communication
+                  Engineering{" "}
                 </td>
-                <td class='text-center'>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_55bc14c7-d3f3-456a-be94-811d1fb94384_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_5b17eeeb-ebbb-45fa-bec5-51fe00905818_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
               </tr>
               <tr>
-                <td class='text-center'>3</td>
-                <td>
-                  B.Tech Third Year Syllabus Computer Science Engineering{' '}
-                </td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_e61919d3-66ef-4494-a3d3-ee6959a445c3_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>4</td>
+                <td class="text-center">3</td>
                 <td>Ordinance Rules &amp; Regulations for B.Tech Programme</td>
-                <td class='text-center'>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_9ab4ee8a-319b-46d7-afbb-86dfe636b0ed_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_09e25b20-5fb3-461f-bbb3-2d864c0f636e_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
               </tr>
               <tr>
-                <td class='text-center'>5</td>
-                <td>MCA First Semester Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_ca2dc4ea-bb0f-46f0-bce0-d403fbd590c2_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>6</td>
-                <td>MCA Second Semester Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_459431d0-54c0-4e05-8e6f-5ab31ffe2c63_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>7</td>
-                <td>MCA Third &amp; Fourth Semester Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_18f93386-a313-4c51-a19d-fe3153307284_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>8</td>
-                <td>BCA Second Semester Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_bb4ba23c-316f-4242-857c-4769eea73ce8_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>9</td>
-                <td>BCA First Semester Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_ac5c363c-6ef0-44d1-ad6e-a4b1093909fa_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>10</td>
-                <td>BCA Third &amp; Fourth Semester Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_db647d37-c681-4b03-92da-dac4599efa38_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>11</td>
+                <td class="text-center">4</td>
                 <td>
-                  B.Tech Fourth Year Syllabus Computer Science &amp; Engineering
+                  B.Tech Second Year Syllabus Electronics and Communication
+                  Engineering
                 </td>
-                <td class='text-center'>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_5998a45d-fbb7-405d-9b0d-6016888d3a8a_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_d6015c64-8887-4ba8-9920-80bc6991d22a_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
               </tr>
               <tr>
-                <td class='text-center'>12</td>
-                <td>MCA Third Year Syllabus</td>
-                <td class='text-center'>
+                <td class="text-center">5</td>
+                <td>
+                  B.Tech Fourth Year Syllabus Electronics and Communication
+                  Engineering
+                </td>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_d0b740c2-6676-4480-80e4-99962ac85e5c_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_f1c23a3a-54f0-49f7-9f0a-fadd64f517c6_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
               </tr>
               <tr>
-                <td class='text-center'>13</td>
-                <td>MCA Two Years CBCS Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_d978a58a-a77b-4f8d-8082-b7128a3ab559_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>14</td>
-                <td>BCA Third Year Syllabus</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_b17731cf-4ccb-4865-9395-d09022944ac2_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>15</td>
+                <td class="text-center">6</td>
                 <td>Open Elective Syllabus</td>
-                <td class='text-center'>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_a91dfbac-d9ac-4353-ba7d-f512d44e93bd_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_d18776d8-da94-42ff-990e-e8cfa3bd0810_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
               </tr>
               <tr>
-                <td class='text-center'>16</td>
-                <td>POs-PSOs-COs of Master of Computer Application (MCA)</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_086b8674-441f-41a3-9c3e-f9c2ba64a609_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>17</td>
+                <td class="text-center">7</td>
                 <td>
-                  POs-PSOs-COs of B.Tech (Computer Science &amp; Engineering)
+                  POs-PSOs-COs of B.Tech (Electronics &amp; Communication
+                  Engineering)
                 </td>
-                <td class='text-center'>
+                <td class="text-center">
                   <a
                     className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_a63f0de1-25f1-4ed2-8833-a3036aac420a_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    View
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td class='text-center'>18</td>
-                <td>POs-PSOs-COs of BCA (Bachelor of Computer Application)</td>
-                <td class='text-center'>
-                  <a
-                    className='text-dark'
-                    href='http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_ccb5cce0-bd82-4ee7-8c2e-9f26ae0fb01d_58.pdf'
-                    target='_blank'
-                    rel='noopener noreferrer'>
+                    href="http://udrc.lkouniv.ac.in//Content/DepartmentContent/SY_547d6ecd-d94e-4579-a20c-6c55ff179e43_57.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     View
                   </a>
                 </td>
@@ -271,7 +137,7 @@ const CourseSyllabus = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseSyllabus
+export default CourseSyllabus;
